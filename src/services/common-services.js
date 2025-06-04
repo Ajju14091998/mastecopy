2667cafd3a9db24970440a458d29149b43995a7a
= <=>
2025-06-04 17:48:50 +0530

    added orders api services

--- src/services/common-services.js
+++ src/services/common-services.js
@@ -32,9 +32,33 @@ const fetchProductList = async (data) => {
   }
 };
 
+const addOrderApi = async (data) => {
+  try {
+    const response = await api.post('myorders/addorder', {...data});
+    console.log('add Order Response -', response);
+    return response.data;
+  } catch (error) {
+    console.error('Failed to add order:', error);
+    return [];
+  }
+};
+
+const fetchOrdersList = async (data) => {
+  try {
+    const response = await api.post('myorders/getmyorders', {...data});
+    console.log('fetchOrdersList Response -', response);
+    return response.data;
+  } catch (error) {
+    console.error('Failed to fetch order list:', error);
+    return [];
+  }
+};
+
 
 export {
   fetchSubCategories,
   fetchFilterList,
   fetchProductList,
+  fetchOrdersList,
+  addOrderApi,
 };
